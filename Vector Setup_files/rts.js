d581cee3916dc31f12689d56a8258e91cccfb4d2
Hello <134814825+cozmo14047@users.noreply.github.com>
2024-10-06 10:15:27 +1300

    Update rts.js

--- Vector Setup_files/rts.js
+++ Vector Setup_files/rts.js
@@ -1217,7 +1217,12 @@
           console.error("Unknown phase:", phase);
       }
 
-      
+      // Limpiar y activar el contenedor correspondiente
+      $(".vec-container.vec-current").css("opacity", 0);
+      $(".vec-container.vec-current").removeClass("vec-current");
+      $("#" + phase).addClass("vec-current");
+      $("#" + phase).css("opacity", 1);
+    }
 
 
 
